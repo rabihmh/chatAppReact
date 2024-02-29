8ddbfa906afa7cf2097f952de7744e32f1912517
rabihmh <76842351+rabihmh@users.noreply.github.com>
2024-02-29 15:47:27 +0200

    design

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -2,8 +2,49 @@ import React from 'react';
 import { Row,Col } from 'react-bootstrap';
 import MessageContainer from './MessageContainer';
 import SendMessageForm from './SendMessageForm';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBIcon,
+  MDBTextArea,
+} from "mdb-react-ui-kit";
 const ChatRoom = ({messages,sendMessage}) => {
     return (
+        <MDBContainer className="py-5">
+          <MDBRow className="d-flex justify-content-center">
+            <MDBCol md="8" lg="6" xl="4">
+              <MDBCard id="chat1" style={{ borderRadius: "15px" }}>
+                <MDBCardHeader
+                  className="d-flex justify-content-between align-items-center p-3 bg-info text-white border-bottom-0"
+                  style={{
+                    borderTopLeftRadius: "15px",
+                    borderTopRightRadius: "15px",
+                  }}
+                >
+                  <MDBIcon fas icon="angle-left" />
+                  <p className="mb-0 fw-bold">Live chat</p>
+                  <MDBIcon fas icon="times" />
+                </MDBCardHeader>
+    
+                <MDBCardBody>
+                  <MessageContainer messages={messages}/>
+                  <SendMessageForm sendMessage={sendMessage}/>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      );
+}
+export default ChatRoom;
+
+
+/*
+ return (
         <div>
            <Row className="px-5 py-5">
                 <Col sm={10}>
@@ -23,5 +64,4 @@ const ChatRoom = ({messages,sendMessage}) => {
             </Row>
         </div>
     );
-}
-export default ChatRoom;
+*/
--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -1,17 +1,23 @@
 import React from 'react';
-
-const MessageContainer = ({messages}) => {
+import '../App.css';
+const MessageContainer = ({ messages }) => {
     return (
         <div>
-                {messages.map((message,index) => (
-                <table striped bordered hover>
-                        <tr key={index}>
-                                <td>{message.msg} - {message.username}</td>
-                        </tr>
-                </table>
-                ))}
+            {messages.map((message, index) => (
+                <div className="message-container" key={index}>
+                    <div className="avatar-circle" style={{ backgroundColor: "#6a5acd" }}>
+                        <span>{message.username.substring(0, 2)}</span>
+                    </div>
+                    <div className="message-content">
+                        <p className="small mb-0">
+                            {message.msg}
+                        </p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
+
 
 export default MessageContainer;
